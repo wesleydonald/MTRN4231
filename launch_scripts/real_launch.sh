872f6d152e63a6c7f4ead2c2db435a1791f3fd0c
Ryan <ryan103a@gmail.com>
2025-12-04 00:40:22 +1100

    changed launch scripts to wait for a key press before launching nodes

--- launch_scripts/real_launch.sh
+++ launch_scripts/real_launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-gnome-terminal -t 'MoveItServer' -e "ros2 launch arm moveit_real.launch.py"
-gnome-terminal -t 'DriverServer' -e "ros2 launch arm ur_control_real.launch.py"
-gnome-terminal -t 'Ros2Nodes' -e "ros2 launch brain real.launch.py"
+gnome-terminal -t 'MoveItServer' -- ros2 launch arm moveit_real.launch.py
+gnome-terminal -t 'DriverServer' -- ros2 launch arm ur_control_real.launch.py
+
+read -p "Press enter to launch ros nodes."
+
+gnome-terminal -t 'Ros2Nodes' -- ros2 launch brain real.launch.py
